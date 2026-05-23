db304101a43cfa46e515915807fbb4fdabf97786
Geo Cobos <geo.cobosd@gmail.com>
2026-05-23 09:38:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,23 @@ st.sidebar.title("Parámetros")
 
 st.sidebar.image("LogoPhyton.png")
 
-uploaded_files = st.file_uploader(
-    "Upload data", accept_multiple_files=True, type="csv"
-)
-for uploaded_file in uploaded_files:
-    df = pd.read_csv(uploaded_file)
-    st.write(df)
+modulo = st.selectbox("seleccione un módulo", [ "Modulo 1"] , [ "Modulo 2"] , [ "Modulo 3"])
 
-monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
-interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
-anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
-numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
+if modulo == "Modulo 1":    
+    uploaded_files = st.file_uploader(
+        "Upload data", accept_multiple_files=True, type="csv"
+    )
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
+        
+elif modulo == "Modulo 3"
+    monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
+    interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
+    anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
+    numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
+    cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
+    st.write("Su cuota mensual es: ",cuota)
+else
+     st.write("se encuentra en el modulo 3")
 
-cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
-st.write("Su cuota mensual es: ",cuota)
