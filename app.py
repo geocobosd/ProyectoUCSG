f194745f5b8c7b19ebc03e1b41a92b3a44b36bed
Geo Cobos <geo.cobosd@gmail.com>
2026-05-23 09:55:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ st.sidebar.image("LogoPhyton.png")
 
 modulo = st.sidebar.selectbox("Seleccione un módulo", ["Módulo 1", "Módulo 2", "Módulo 3"] )
 
-if modulo == "Modulo 1":    
+if modulo == "Módulo 1":    
     uploaded_files = st.file_uploader(
         "Upload data", accept_multiple_files=True, type="csv"
     )
     for uploaded_file in uploaded_files:
         df = pd.read_csv(uploaded_file)
         st.write(df)
-elif modulo == "Modulo 3":
+elif modulo == "Módulo 2":
     monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
     interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
     anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
